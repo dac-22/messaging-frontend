92254fb3dd5a63e0f7ff13b314179be2a9820997
santoshmondal <santosh.ece06@gmail.com>
2022-12-17 14:35:40 +0530

    emojis

--- 3.js
+++ 3.js
@@ -1,5 +1,6 @@
 window.addEventListener("load", () => {
   loadMessages();
+  loadEmojis();
 });
 
 async function addMessageToServer(reply) {
@@ -39,7 +40,7 @@ async function loadMessages() {
 
     let newElement = `<div class="d-flex ${dclass}">
                         <div class="d-flex justify-content-between align-items-center my-1">
-                          <div class="badge text-bg-secondary fs-6">
+                          <div class="badge text-bg-secondary fs-5">
                             ${item.message}
                             <span class="ms-4" style="font-size: 10px">${hour}:${minute} pm</span>
                           </div>
@@ -49,4 +50,25 @@ async function loadMessages() {
     parent.innerHTML = newElement + parent.innerHTML;
   }
   console.log(response);
+}
+
+function loadEmojis() {
+  let ref = document.querySelector("#emojiId1");
+
+  for (let i = 0; i < 75; i++) {
+    let counter = 12 + i;
+    let newElement = `<span onclick="addEmoji(this)" class="fs-1" data-value="1285${counter}">&#1285${counter};</span>`;
+    ref.innerHTML += newElement;
+  }
+}
+
+function addEmoji(currentRef) {
+  let emojicode = `&#${currentRef.dataset.value};`;
+  let textRef = document.querySelector("#textId1");
+  textRef.value = emojicode;
+  addMessageToServer(false);
+
+  let collapseRef = document.querySelector("#collapseExample");
+  collapseRef.classList.remove("show");
+  collapseRef.classList.add("hide");
 }
